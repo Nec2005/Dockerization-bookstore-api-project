c53655e6a78e1b109faf997bdadffc2a9011ad07
Nec2005 <necatiunal2005@gmail.com>
2022-01-22 16:11:14 +0000

    bookstore-api-project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Dockerization-bookstore-api-project
-Bookstore Web API Application
+Bookstore Web API Application. The application code is to be deployed as a RESTful web service with Flask using Dockerfile and Docker Compose on AWS Elastic Compute Cloud (EC2) Instance using Terraform
--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "3.72.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
     }
   }
 }
@@ -12,16 +16,47 @@ provider "aws" {
 }
 
 data "aws_ami" "linux2" {
-  owners = ["amazon"]
-  most_recent = 
+  owners      = ["amazon"]
+  most_recent = true
   filter {
-      name = "name"
-      values = ["amzn2-ami-hvm*"]
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
   }
+}
+provider "github" {
+  token = "XXXXXXXXXXXX"
+}
+
+
+resource "github_repository" "myrepo" {
+  name       = "bookstore-api-project"
+  auto_init  = true
+  visibility = "private"
+}
+
+resource "github_branch_default" "main" {
+  branch     = "main"
+  repository = github_repository.myrepo.name
+}
+
+variable "files" {
+  default = ["bookstore-api.py", "docker-compose.yml", "Dockerfile", "requirements.txt"]
+
+}
+
+
+resource "github_repository_file" "app_files" {
+  for_each            = toset(var.files)
+  content             = file(each.value)
+  file                = each.value
+  repository          = github_repository.myrepo.name
+  overwrite_on_create = true
+  branch              = "main"
+  commit_message      = "app-files added to repo"
 }
 
 resource "aws_security_group" "tf-secgrp" {
-  name = "allow_https_ssh"
+  name = "tf-secgrp"
   tags = {
     Name = "allow_ssh_https"
   }
@@ -53,11 +88,14 @@ resource "aws_security_group" "tf-secgrp" {
 }
 
 resource "aws_instance" "apiserver" {
-    ami = data.aws_ami.linux2
-    instance_type = "t2.micro"
-    key_name = "firstkey"
-    security_groups = ["${aws_security_group.tf-secgrp}"]
-    user_data = <<-EOF
+  ami             = data.aws_ami.linux2.id
+  instance_type   = "t2.micro"
+  key_name        = "firstkey"
+  security_groups = ["tf-secgrp"]
+  tags = {
+    Name = "Web Server of Bookstore"
+  }
+  user_data = <<-EOF
                 #!/bin/bash
                 yum update -y
                 yum install git -y
@@ -68,12 +106,23 @@ resource "aws_instance" "apiserver" {
                 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
                 -o /usr/local/bin/docker-compose
                 chmod +x /usr/local/bin/docker-compose
-                git clone https://github.com/Nec2005/Dockerization-bookstore-api-project.git
-                cd /home/ec2-user/Dockerization-bookstore-api-project
-                docker-compose up
+                mkdir -p /home/ec2-user/bookstore-api
+                TOKEN="XXXXXXXXXXXX"
+                FOLDER="https://$TOKEN@raw.githubusercontent.com/Nec2005/bookstore-api-project/main/"
+                curl -s --create-dirs -o "/home/ec2-user/bookstore-api/app.py" -L "$FOLDER"bookstore-api.py
+                curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
+                curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
+                curl -s --create-dirs -o "/home/ec2-user/bookstore-api/docker-compose.yml" -L "$FOLDER"docker-compose.yml
+                cd /home/ec2-user/bookstore-api
+                docker build -t nec2005/bookstoreapi:latest .
+                docker-compose up -d
                 EOF
-
-    tags = {
-        Name = "apiserver"
-    }
+    depends_on = [github_repository.myrepo, github_repository_file.app_files]
 }
+
+output "webserver" {
+  value = "http://${aws_instance.apiserver.public_dns}"
+
+}
+
+
